2cf7c01478059282945643a135e25f359488becc
Tessa Delsener <wildtessa.70@icloud.com>
2024-04-27 16:58:11 -0700

    Initial Commit

---
+++ first user input app/first_user_input_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_user_input_appApp.swift
+//  first user input app
+//
+//  Created by Tessa Delsener on 4/27/24.
+//
+
+import SwiftUI
+
+@main
+struct first_user_input_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
